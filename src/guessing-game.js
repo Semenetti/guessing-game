5ba9fcbb42dd97bb50e3d456d110faa7333b35ad
Semenetti <adriano.semenetti@gmail.com>
2019-09-26 21:32:42 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,21 @@
 class GuessingGame {
     constructor() {}
-
+    
     setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+        this.leftSide = min, this.rightSide = max;
+        }
+    
+        guess() {    
+        return this.answer = Math.round((this.leftSide+this.rightSide)/2);
+        }
+    
+        lower() {
+        return this.rightSide = this.answer;
+        }
+    
+        greater() {
+        return this.leftSide = this.answer;
+        }
 }
 
 module.exports = GuessingGame;
